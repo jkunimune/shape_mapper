b70372175f66f74057fb64f579344bddc4a28b6a
Justin Kunimune <justinkunimune@gmail.com>
2024-09-05 16:54:50 -0400

    reading YAML from disc
    look at me, I'm a real rustacean now!  now that it's reading from the configuration file, I should figure out how to make it write to the image file.

--- source/main.rs
+++ source/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("map");
+use std::fs;
+use serde::{Deserialize};
+
+fn main() -> () {
+    let yaml = fs::read_to_string(
+        "configurations/congresentatives.yml").unwrap();
+    let configuration: Configuration = serde_yaml::from_str(&yaml).unwrap();
+    println!(
+        "\
+<svg width=\"{}\" height=\"{}\">
+    <title>{}</title>
+    <style>
+{}
+    </style>
+</svg>
+        ",
+        configuration.bounding_box.right - configuration.bounding_box.left,
+        configuration.bounding_box.bottom - configuration.bounding_box.top,
+        configuration.title, configuration.style);
+}
+
+#[derive(Deserialize)]
+struct Configuration {
+    title: String,
+    style: String,
+    bounding_box: Box,
+}
+
+#[derive(Deserialize)]
+struct Box {
+    left: f64,
+    right: f64,
+    top: f64,
+    bottom: f64,
 }
