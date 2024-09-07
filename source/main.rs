5806ff11830ed52140829ccaa6e7be393f632cb9
Justin Kunimune <justinkunimune@gmail.com>
2024-09-07 11:40:27 -0400

    multiple data
    it can now load multiple shapefiles and sort them into groups!

--- source/main.rs
+++ source/main.rs
@@ -1,7 +1,8 @@
-use std::fs;
-use serde::{Deserialize};
+use std::{fs, iter};
+use serde::Deserialize;
 use shapefile::dbase::FieldValue;
 use shapefile::Shape;
+
 
 fn main() -> () {
     let yaml = fs::read_to_string(
@@ -20,42 +21,9 @@ fn main() -> () {
         configuration.bounding_box.bottom - configuration.bounding_box.top,
         configuration.title, configuration.style,
     );
-
-    svg_code.push_str(&format!("  <g class=\"{}\">\n", configuration.content.class));
-    let mut reader = shapefile::Reader::from_path(format!("data/{}.shp", configuration.content.filename)).unwrap();
-    for shape_record in reader.iter_shapes_and_records() {
-        let shape_record = shape_record.unwrap();
-        let (shape, record) = shape_record;
-        let identifier: Option<String> = match record.get("DIST_NUM") {
-            Some(value) => match value {
-                FieldValue::Character(character) => character.clone(),
-                FieldValue::Numeric(number) => match number {
-                    Some(number) => Some(format!("{}-{}", configuration.content.class, number)),
-                    None => None,
-                },
-                _ => None,
-            },
-            None => None,
-        };
-        match shape {
-            Shape::Polygon(polygon) => {
-                let mut path_string = String::new();
-                for ring in polygon.rings() {
-                    for (i, point) in ring.points().iter().enumerate() {
-                        path_string.push_str(&format!("{}{:.3},{:.3} ", if i == 0 {"M"} else {"L"}, point.x, point.y));
-                    }
-                }
-                svg_code.push_str(&format!(
-                    "    <path {}d=\"{}\" />\n",
-                    match identifier { Some(string) => format!("class=\"{}\" ", string), None => String::from("")},
-                    path_string));
-            }
-            _ => {
-                panic!("we only do polygons right now.");
-            }
-        }
+    for content in configuration.content {
+        svg_code.push_str(&transcribe_as_svg(&content, 1).unwrap());
     }
-    svg_code.push_str("  </g>\n");
     svg_code.push_str("</svg>\n");
 
     let image_filename = "maps/congresentatives.svg";
@@ -63,13 +31,73 @@ fn main() -> () {
     fs::write(image_filename, svg_code).unwrap();
 }
 
+
+fn transcribe_as_svg(content: &Content, indent_level: usize) -> Result<String, shapefile::Error> {
+    let indentation: String = iter::repeat("  ").take(indent_level).collect();
+    let parameters = match content {
+        Content::Group{ id, .. } => &format!("id=\"{}\"", id),
+        Content::Layer{ class, .. } => &format!("class=\"{}\"", class),
+    };
+    let mut string: String = String::from(format!("{}<g {}>\n", &indentation, parameters));
+    match content {
+        Content::Group{ content: subcontents, .. } => {
+            for subcontent in subcontents {
+                string.push_str(&transcribe_as_svg(subcontent, indent_level + 1)?);
+            }
+        }
+        Content::Layer{ filename, class } => {
+            let mut reader = shapefile::Reader::from_path(
+                format!("data/{}.shp", filename))?;
+            for shape_record in reader.iter_shapes_and_records() {
+                let shape_record = shape_record.unwrap();
+                let (shape, record) = shape_record;
+                let identifier: Option<String> = match record.get("DIST_NUM") {
+                    Some(value) => match value {
+                        FieldValue::Character(character) => character.clone(),
+                        FieldValue::Numeric(number) => match number {
+                            Some(number) => Some(format!("{}-{}", class, number)),
+                            None => None,
+                        },
+                        _ => None,
+                    },
+                    None => None,
+                };
+                let class_string = match identifier {
+                    Some(string) => format!("class=\"{}\" ", string),
+                    None => String::from("")
+                };
+                match shape {
+                    Shape::Polygon(polygon) => {
+                        let mut path_string = String::new();
+                        for ring in polygon.rings() {
+                            for (i, point) in ring.points().iter().enumerate() {
+                                path_string.push_str(&format!("{}{:.3},{:.3} ", if i == 0 {"M"} else {"L"}, point.x, point.y));
+                            }
+                        }
+                        string.push_str(&format!(
+                            "{}  <path {}d=\"{}\" />\n",
+                            indentation, class_string, path_string));
+                    }
+                    _ => {
+                        panic!("we only do polygons right now.");
+                    }
+                }
+            }
+        }
+    }
+    string.push_str(&format!("{}</g>\n", &indentation));
+    return Ok(string);
+}
+
+
 #[derive(Deserialize)]
 struct Configuration {
     title: String,
     style: String,
     bounding_box: Box,
-    content: Layer,
+    content: Vec<Content>,
 }
+
 
 #[derive(Deserialize)]
 struct Box {
@@ -79,8 +107,15 @@ struct Box {
     bottom: f64,
 }
 
+
 #[derive(Deserialize)]
-struct Layer {
-    class: String,
-    filename: String,
+enum Content {
+    Layer {
+        class: String,
+        filename: String
+    },
+    Group {
+        id: String,
+        content: Vec<Content>
+    },
 }
